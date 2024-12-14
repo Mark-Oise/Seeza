f0fb8ff3b9d8a315a638093d604869e1d22e94ba
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-12-14 06:00:17 +0100

    some changes

--- config/asgi.py
+++ config/asgi.py
@@ -1,14 +1,14 @@
 import os
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-# Initialize Django
-django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from apps.chat.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
